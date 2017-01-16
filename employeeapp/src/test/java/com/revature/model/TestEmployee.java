ad32988daaaa00ace5e566dbebaf3066b82dff8d
Revature <Revature@Revature96589>
2017-01-16 06:52:15 -0500

    Model

---
+++ employeeapp/src/test/java/com/revature/model/TestEmployee.java
@@ -0,0 +1,16 @@
+package com.revature.model;
+
+import com.revature.model.Employee;
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Employee employee=new Employee();
+ employee.setId(2);
+ employee.setName("aravind");
+ employee.setSalary(1000);
+ System.out.println(employee);
+	}
+
+}
